09b1e00b95cfa84a5f791e33d32649231a035185
jelenu <jesusleon2700@gmail.com>
2025-06-18 11:57:36 +0200

    status bar hidden

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
 import { AuthProvider } from './contexts/AuthContext';
 import RootNavigation from './navigation';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
     <AuthProvider>
       <SafeAreaView style={styles.safeArea}>
+        <StatusBar hidden />
         <RootNavigation />
       </SafeAreaView>
     </AuthProvider>
@@ -17,6 +19,5 @@ const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
     backgroundColor: '#fff',
-    paddingTop: 20, // Adjust for status bar height
   },
 });
